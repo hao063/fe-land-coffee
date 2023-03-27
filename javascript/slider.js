2b2cd24d141afd33cf2a35e8e0ad05fe9f7fbec3
Nguyen Hao <nph063@gmail.com>
2023-03-28 00:28:06 +0700

    hád

---
+++ javascript/slider.js
@@ -0,0 +1,5 @@
+let slider = document.getElementsByClassName(".slider")[0];
+let slides = slider.children;
+let indicator = document.getElementsByClassName(".indicator")[0];
+
+console.log(slider, slides, indicator);
